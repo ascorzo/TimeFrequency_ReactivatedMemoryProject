d6945971b9403acd3facd06179694db44874b9bf
David <github@davidb.anonaddy.com>
2021-04-06 13:10:42 -0400

    Initial commits

---
+++ TF_during_SlowOsc/Compute_TF_of_event_series.m
@@ -0,0 +1,359 @@
+% Compute time-frequency around subject's maximum spindle band peak
+
+% -------------------------------------------------------------------------
+% ---------------------------               -------------------------------
+% ---------------------------   N O T E !   -------------------------------
+% ---------------------------               -------------------------------
+% -------------------------------------------------------------------------
+
+% Events' TF are averaged (post-TF computation) for each channel. If you
+% want to maintain individual event TF matrices for each channel, comment
+% out line 229: data_TF_norm.powspctrm  = mean(data_TF_norm.powspctrm, 1);
+
+
+% -------------------------------------------------------------------------
+% ---------------------------               -------------------------------
+% ---------------------------   I N P U T   -------------------------------
+% ---------------------------               -------------------------------
+% -------------------------------------------------------------------------
+
+% 1. File containing information about maximum spindle peaks
+%
+% 2. Slow oscillation time series as prepared by
+%    DataPrep_extract_event_series.m
+%
+% 3. Parameters
+%       - General
+ft_defaults
+ft_warning off
+PM.Conditions           = {'ShamOn', 'OdorOn'};
+% Off periods ignored since baseline done during TF
+PM.ClustOI              = 'all';
+% See Clust list below. Can be 'all' and should be left 'all' since
+% channels of interest can be redefined later in other scripts
+PM.Spindleband          = 'fast';
+PM.FrRange              = 10; % Hz, range of frequencies to take into
+                              % account around spindle band peak 
+%       - Time frequency parameters
+PM.s_tstep              = 0.1; % try with 0.005
+PM.s_fstep              = 0.05; % 0.005
+PM.cycles               = 12;
+PM.cfg_Tf.method        = 'wavelet';
+PM.cfg_Tf.output        = 'pow';
+PM.cfg_Tf.width         = PM.cycles;
+PM.cfg_Tf.toi           = 'all'; % Extended before and after to deal with
+                                 % border effect of wavelet
+PM.cfg_Tf.keeptrials    = 'yes';
+%       - Data window selection
+PM.cfg_seldat.latency   = [-2 2];
+%       - Baseline parameters
+PM.cfg_Bas.baseline      = PM.cfg_seldat.latency;
+% Purposely choosing the whole window as baseline as we must assume that
+% most SOs are preceeded by another SO (S4) and therefore, baseline 
+% calculation with a time window before the SO might neutralize desired
+% effects.
+PM.cfg_Bas.baselinetype  = 'zscore';
+%       - File paths
+filepath = ['D:\germanStudyData\datasetsSETS\Ori_CueNight\', ...
+           'preProcessing\EEGLABFilt_Mastoids_Off_On_200Hz_Oct_NEW\', ...
+           '05-Mar-2021_Cue_NEW\SO_timeSeries\'];
+% filepath = ['/mnt/disk1/sleep/Datasets/CueD_SO_TimeSeires/'];
+savepath = strcat(filepath, 'TF_matrices');
+peakpath = ['D:\Gits\SO_Spindle_Detection_Coupling\', ...
+           'SubjectSpecific\Max_spindlebands_byEye.mat'];
+% peakpath = ['/home/sleep/Documents/DAVID/Gits/', ...
+%             'SO_Spindle_Detection_Coupling/', ...
+%             'SubjectSpecific/Max_spindlebands_byEye.mat'];
+
+
+% -------------------------------------------------------------------------
+% --------------------------                 ------------------------------
+% --------------------------   O U T P U T   ------------------------------
+% --------------------------                 ------------------------------
+% -------------------------------------------------------------------------
+
+% 1. TF_series
+%    Contains the time-frequency matrices and channel information
+%
+% 2. SO_wave
+%    These are the average wave forms by channel
+%
+% 3. PM
+%    Used parameters
+
+
+
+%% Set up userland
+%  ------------------------------------------------------------------------
+
+files = dir(strcat(filepath,'*.mat'));
+
+load(peakpath);
+
+if exist(savepath, 'dir') ~= 7
+    mkdir(savepath)
+end
+
+% Clusters of interest
+% --------------------
+PM.Clust.left_frontal = {...
+    'E15', 'E16', 'E11', 'E18', 'E19', 'E22', 'E23', 'E24', 'E26', ...
+    'E27', 'E33', 'E38'};
+PM.Clust.right_frontal = {...
+    'E15', 'E16', 'E11', 'E10', 'E4', 'E9', 'E3', 'E124', 'E2', ...
+    'E123', 'E122', 'E121'};
+PM.Clust.frontal = {...
+    'E3', 'E4', 'E9', 'E10', 'E11', 'E15', 'E16', 'E18', 'E19', ...
+    'E22', 'E23', 'E24', 'E124'};
+PM.Clust.left_central = {...
+    'E6', 'E7', 'E13', 'E30', 'E31', 'E37', 'E54', 'E55'};
+PM.Clust.right_central = {...
+    'E6', 'E55', 'E112', 'E106', 'E105', 'E80', 'E87', 'E79'};
+PM.Clust.central = {...
+    'E6', 'E7', 'E13', 'E30', 'E31', 'E37', 'E54', 'E55', 'E79', ...
+    'E80', 'E87', 'E105', 'E106', 'E112'};
+PM.Clust.left_temporal = {...
+    'E46', 'E51', 'E45', 'E50', 'E58', 'E56', 'E63'};
+PM.Clust.right_temporal = {...
+    'E108', 'E102', 'E101', 'E97', 'E96', 'E99', 'E107'};
+PM.Clust.left_parietal = {...
+    'E53', 'E61', 'E62', 'E72', 'E67', 'E52', 'E60', 'E59', 'E66', ...
+    'E65', 'E64', 'E68'};
+PM.Clust.right_parietal = {...
+    'E62', 'E72', 'E78', 'E77', 'E86', 'E85', 'E84', 'E92', 'E91', ...
+    'E90', 'E95', 'E94'};
+PM.Clust.parietal = {...
+    'E52', 'E61', 'E62', 'E59', 'E60', 'E67', 'E66', 'E72', 'E78', ...
+    'E77', 'E86', 'E85', 'E84', 'E92', 'E91', 'E53'};
+PM.Clust.left_occipital = {...
+    'E71', 'E70', 'E75', 'E74', 'E69', 'E73'};
+PM.Clust.right_occipital = {...
+    'E75', 'E76', 'E82', 'E83', 'E88', 'E89'};
+PM.Clust.occipital = {...
+    'E71', 'E70', 'E74', 'E69', 'E73', 'E75', 'E76', 'E83', 'E82', ...
+    'E89', 'E88'};
+
+if ~strcmp(PM.ClustOI, 'all')
+    Cluster = PM.Clust.(PM.ClustOI);
+    % Otherwise, will be defined later when a subject file has been loaded
+    % containing channel information.
+end
+
+
+
+%% Fruitloops
+%  ------------------------------------------------------------------------
+
+for i_subj = 1:numel(files)
+    
+    % Load Data
+    if ~exist('file_loaded', 'var') || file_loaded == 0
+        SOseries = load([filepath, files(i_subj).name]);
+        % Has to be loaded into new variable, otherwise PM will get
+        % overwritten.
+        PM.filename = [filepath, files(i_subj).name];
+        file_loaded = 1; %#ok<NASGU>
+    end
+    
+    
+    % If ClusterOI set to all, we define this here
+    if strcmp(PM.ClustOI, 'all')
+        Cluster = fieldnames(SOseries.SO_timeSeries)';
+    end
+    
+    
+    % Determine frequency range to analyze for subject
+    % Get subjects spindle peak to center freq range of TF around
+    subject_short    = extractBefore(files(i_subj).name, '_sleep');
+    idx_spindle_peak = find(strcmp({spindle_max.subjects}, subject_short));
+    
+    PM.peakFr        = spindle_max(idx_spindle_peak).(PM.Spindleband);
+    
+    v_freqs          = ...
+        PM.peakFr-PM.FrRange/2:PM.s_fstep:PM.peakFr+PM.FrRange/2;
+    
+    
+    for condition = PM.Conditions
+        
+        
+        % We will store the average wave by channel of cluster
+        SO_wave_chans       = NaN(numel(Cluster), ...
+                              diff(PM.cfg_seldat.latency * ...
+                              SOseries.PM.Info.TrialParameters.s_fs));
+        TF_condition        = struct(); % By far the heaviest variable here
+        TF_condition_restr  = struct();
+        
+        % Used for determining the size of power spectrum matrix when
+        % ctenating TF matrices
+        numTrials           = NaN(1, numel(Cluster));
+        
+        
+        for i_chan = 1:numel(Cluster)
+            % There was probably a better way to prepare data, but the way
+            % it has been done (*) forces us to do by channel separately.
+            % Reason for this was that each channel would have different
+            % numbers of trials. Does this interfere during TF?
+            % (* every condition and every channel in separate field)
+            
+            channel             = char(Cluster(i_chan));
+            
+            disp(strcat('Subject:',     {' '},    files(i_subj).name))
+            disp(strcat('Condition:',   {' '},    condition))
+            disp(strcat('Channel:',     {' '},    channel))
+            
+            
+            % Select data channels according to condition and channel
+            data_raw = SOseries.SO_timeSeries.(channel).(char(condition));
+            
+            
+            
+            %% Computing and processing time-frequency spectrum
+            %  ------------------------------------------------------------
+            
+            % Time-Frequency Calculation
+            PM.cfg_Tf.foi       = v_freqs;
+            data_TF             = ft_freqanalysis(PM.cfg_Tf, data_raw);
+            
+            
+            % Select time of interest removing borders
+            data_TF_bas         = ft_selectdata(PM.cfg_seldat, data_TF);
+            
+            % Baseline correction
+            data_TF_norm        = ft_freqbaseline(PM.cfg_Bas, data_TF_bas);
+            
+            
+            % Store results in channel structure
+            % Without meaning, script gave OOM errors after some time since
+            % TF_condition will be around 14GB! We have no choice but to 
+            % mean the events' TF matrices at this stage already.
+            data_TF_norm.powspctrm  = mean(data_TF_norm.powspctrm, 1);
+            TF_condition.(channel)  = data_TF_norm;
+            
+            
+            % Used for creating catenated data. If meaned in line 229, all 
+            % channels will be 1 in trials.
+            numTrials(i_chan) = size(data_TF_norm.powspctrm, 1);
+            
+            
+            
+            %% Extract average wave form of slow osc. of channel
+            %  ------------------------------------------------------------
+            
+            SO_wave_trl =   NaN(numTrials(i_chan), ...
+                            SOseries.PM.s_timeWindow * ...
+                            SOseries.PM.Info.TrialParameters.s_fs);
+            
+            for i_trl = 1:numTrials(i_chan)
+                SO_wave_trl(i_trl, :) = data_raw.trial{i_trl};
+            end
+            
+            % -------------------------------------------------------------
+            % The window of the wave form was the length of the raw data
+            % which had extended times at both orders in order to
+            % circumvent TF border effects. We need to strip these borders
+            % from the wave form time series here.
+            SO_wave_wholeWindow = mean(SO_wave_trl, 1);
+            
+            v_wave_times_whole = ...
+                - length(SO_wave_wholeWindow) / 2 + 1 : 1 : ...
+                length(SO_wave_wholeWindow) / 2;
+            
+            v_wave_times = ...
+                PM.cfg_seldat.latency(1) * ...
+                SOseries.PM.Info.TrialParameters.s_fs + 1 : 1 : ...
+                PM.cfg_seldat.latency(2) * ...
+                SOseries.PM.Info.TrialParameters.s_fs;
+            % -------------------------------------------------------------
+            
+            SO_wave_chans(i_chan, :) = ...
+            SO_wave_wholeWindow(...
+            ismember(v_wave_times_whole, v_wave_times));
+            
+        end
+        
+        
+        %% Restructure data so that channels are inside same array
+        %  ----------------------------------------------------------------
+        
+        % This allows for easier data preparation when calling
+        % permutation-based stats.
+        
+        % /!\ Could not get it done with ft_appenddata ...
+        
+        % TF_condition_restr
+        %   cfg             = various information about data processing
+        %   label           = Cell(channel labels x 1)
+        %   freq            = 1 x frequency vector output
+        %   time            = 1 x time vector
+        %   dimord          = 'rpt_chan_freq_time'
+        %   elec
+        %       chanpos     = coordinates of channel [channels by x, y, z]
+        %       chantype    = repmat({'eeg'}, numel(Cluster), 1)
+        %       chanunit    = repmat({'V'}, numel(Cluster), 1)
+        %       elecpos     = TF_condition_restr.elec.chanpos
+        %       label       = TF_condition_restr.label' (permutated)
+        %       unit        = 'cm'
+        %   trialinfo       = Event triggers (vary between channels.
+        %                     Therefore, take this field out)
+        %   powspctrm       = rpt_chan_freq_time matrix, this is where we
+        %                     canetate trials in dimension 2
+        
+        
+        TF_condition_restr.label  = Cluster';
+        TF_condition_restr.cfg    = TF_condition.(char(Cluster(1))).cfg;
+        TF_condition_restr.freq   = TF_condition.(char(Cluster(1))).freq;
+        TF_condition_restr.time   = TF_condition.(char(Cluster(1))).time;
+        TF_condition_restr.dimord = TF_condition.(char(Cluster(1))).dimord;
+        
+        TF_condition_restr.powspctrm =  NaN(...
+                                        max(numTrials), ...
+                                        numel(Cluster), ...
+                                        numel(TF_condition_restr.freq), ...
+                                        numel(TF_condition_restr.time));
+        TF_condition_restr.elec = [];
+        for i_chan = 1:numel(Cluster)
+            TF_condition_restr.powspctrm(...
+                1:numTrials(i_chan), i_chan, :, :) = ...
+                TF_condition.(char(Cluster(i_chan))).powspctrm;
+            
+            TF_condition_restr.elec = [TF_condition_restr.elec; ...
+                TF_condition.(char(Cluster(i_chan))).elec];
+        end
+
+        % -----------------------------------------------------------------
+        % ------------------   I M P O R T A N T   ------------------------
+        %
+        % Should we randomly select trials and balance the trial number 
+        % over channels? Right now, every channel has its own number of 
+        % trials based on how many slow osc. were detected.
+        % Does not seem necessary, since what we feed to ft_freqstatistics
+        % are the subjects' means over their respective trials for each 
+        % channel. Therefore, the dimension rpt in the input array for 
+        % ft_statistics will be subjects (x channels x freqs x times) where
+        % the initially different amount of trials per subject does not
+        % matter since we will mean over them to create a subject rpt.
+        % -----------------------------------------------------------------
+        
+        SO_wave.(char(condition))   = SO_wave_chans;
+        TF_series.(char(condition)) = TF_condition_restr;
+    end
+
+    
+    
+    %% Parsing last parameters to output and save results for subjects
+    %  --------------------------------------------------------------------
+    
+    PM_datapreparation  = SOseries.PM;
+    PM.Clust.all        = SOseries.PM.Info.ROIs.str_chans;
+    PM.Info             = SOseries.PM.Info;
+    save([savepath, filesep, subject_short, '_TF.mat'], ...
+        'TF_series', 'SO_wave', 'PM', '-v7.3')
+    % Has to be saved as Version 7.3 because bigger than 2GB. This means
+    % generated files will not be Octave-compatible.
+    
+    file_loaded = 0; % Load next subject file
+end
+
+% Had a weird error once atthe very end of the script. Make sure here that
+% everything went through!
+fprintf('\nScript is done!\n')
---
+++ TF_during_SlowOsc/Stats_over_TF_of_event_series.m
@@ -0,0 +1,306 @@
+% Compute cluster-based statistical analysis over subjects time-frequencies
+
+% -------------------------------------------------------------------------
+% ---------------------------               -------------------------------
+% ---------------------------   N O T E !   -------------------------------
+% ---------------------------               -------------------------------
+% -------------------------------------------------------------------------
+
+% As of now, the script is non-functional and still in work!
+
+
+% -------------------------------------------------------------------------
+% ---------------------------               -------------------------------
+% ---------------------------   I N P U T   -------------------------------
+% ---------------------------               -------------------------------
+% -------------------------------------------------------------------------
+
+% 1. Files by subject containing time series by channel and by condition 
+%    prepared by Compute_TF_of_event_series.m as well as the average wave
+%    form of the time window
+%       - PM        = parameters used during time-frequency computation
+%       - TF_series = time-freq matrices
+%       - SO_wave   = raw amplitude of event time window
+%
+% 2. Parameters
+%       - File paths
+seriespath          = ['D:\germanStudyData\datasetsSETS\', ...
+                        'Ori_CueNight\preProcessing\', ...
+                        'EEGLABFilt_Mastoids_Off_On_200Hz_Oct_NEW\', ...
+                        '05-Mar-2021_Cue_NEW\SO_timeSeries\TF_matrices\'];
+%       - Channel cluster
+PM.ClustOI          = 'left_occipital';
+% Cell array of channels of interest. See Cluster list below. Can also be
+% 'all' to include all channels
+%       - Conditions to analyze
+PM.Conditions       = {'ShamOn', 'OdorOn'};
+%       - channel locations in fieldtrip structure
+chanlocspath        = ['D:\Gits\Spindle_analysis\subjectsChanlocs\', ...
+                        'fieldtrip_chanlocs.mat'];
+
+
+
+%% Set up userland
+%  ------------------------------------------------------------------------
+
+files = dir(strcat(seriespath,'*.mat'));
+
+% Dummy file is important because most parameters will be read from this
+% file
+if ~exist('dummy_loaded', 'var') || dummy_loaded == 0
+    dummyFile = load([seriespath, files(1).name]);
+    dummy_loaded = 1;
+end
+
+% Define channels to go through
+Cluster = dummyFile.PM.Clust.(PM.ClustOI)';
+
+% TF_dimensions = trials by channels by freqs by times
+v_TF_dimensions = ...
+    size(dummyFile.TF_series.(char(PM.Conditions(1))).powspctrm);
+% First dimension varies between subjects, but it does not matter as this
+% will never be taken into account during this script. We just average over
+% all trials (events).
+
+% Prepare subject time-freq matrix
+for condition = PM.Conditions
+    TF_subject.(char(condition)).powspctrm = NaN(...
+        length(files), ...
+        numel(Cluster), ...
+        v_TF_dimensions(3), ...
+        v_TF_dimensions(4));
+end
+
+% Prepare subject wave form series
+s_windowSize = ...
+    diff(dummyFile.PM.cfg_seldat.latency) * ...
+    dummyFile.PM.Info.TrialParameters.s_fs;
+for condition = PM.Conditions
+    WF_subject.(char(condition)) = NaN(length(files), s_windowSize);
+end
+
+
+
+%% Fruitloops
+%  ------------------------------------------------------------------------
+
+% Here, we:
+% 1. Average by subject the TF matrices over channels belonging to cluster
+%    of interest by condition.
+% 2. Catenate all subjects mean matrices to ft_freqstatistics-interpretable
+%    data structure.
+% 3. Extract the average wave form over cluster channels which should
+%    ressemble a theoretical slow oscillation.
+
+for i_subj = 1:length(files)
+    
+    if ~exist('file_loaded', 'var') || file_loaded == 0
+        load([seriespath, files(i_subj).name])
+        file_loaded = 1; %#ok<NASGU>
+    end
+    
+    disp(strcat('Subject:',     {' '},    files(i_subj).name))
+    
+    
+    for condition = PM.Conditions
+        
+          
+        for i_chan = 1:numel(Cluster)
+            
+            channel = Cluster(i_chan);
+            
+            idx_chan = find(strcmp(...
+                dummyFile.PM.Info.ROIs.str_chans, channel));
+            
+            
+            
+            %% Average time series' TF over trials
+            %  ------------------------------------------------------------
+            
+            TF_chan = nanmean(TF_series.(...
+                char(condition)).powspctrm(:, idx_chan, :, :), 1);
+            TF_subject.(char(condition)).powspctrm(i_subj, i_chan, :, :) = ...
+                TF_chan;
+            
+            
+            
+            %% Average wave form of cluster
+            %  ------------------------------------------------------------
+        
+            % /!\ The time series of the wave forms has the borders still
+            % attached that were used to circumvent border effects in TF
+            % computation. We hard-code the time selection here. This has 
+            % been corrected in the TF computation script and therefore, 
+            % v_times here can be erased in future.
+            
+            error(['Andrea, if you have run the Compute_TF_of_event', ...
+                '_series script again, you can remove this time', ...
+                'vector selection here and just take SO_wave(idx_chan, :)'])
+            v_times = 201:1:1000;
+            WF_subject.(char(condition))(i_subj, :) = ...
+                SO_wave.(char(condition))(idx_chan, v_times);
+            
+        end
+        
+    end
+    
+    file_loaded = 0;
+end
+
+
+
+%% Verify TF matrices and wave forms
+%  ------------------------------------------------------------------------
+
+% Find minima and maxima of time-freq matrices
+% --------------------------------------------
+TF_Odor = TF_subject.OdorOn.powspctrm;
+TF_Odor = mean(TF_Odor, 1);
+TF_Odor = mean(TF_Odor, 2);
+TF_Odor = squeeze(TF_Odor);
+
+TF_Sham = TF_subject.ShamOn.powspctrm;
+TF_Sham = mean(TF_Sham, 1);
+TF_Sham = mean(TF_Sham, 2);
+TF_Sham = squeeze(TF_Sham);
+
+minVal = min([TF_Odor(:); TF_Sham(:)]);
+maxVal = max([TF_Odor(:); TF_Sham(:)]);
+
+
+figure
+colororder({'b', 'k'})
+i_plot = 1;
+for condition = PM.Conditions
+    
+    subplot(1, 2, i_plot)
+    
+    % Time frequency plot
+    % -------------------
+    TF_meanChans  = mean(TF_subject.(char(condition)).powspctrm, 2);
+    
+    v_frequencies = 1:length(TF_series.OdorOn.freq);
+    v_frequencies = (max(v_frequencies) - v_frequencies) / ...
+        (max(v_frequencies) - min(v_frequencies));
+    v_frequencies = v_frequencies * PM.FrRange;
+    v_frequencies = v_frequencies - (PM.FrRange / 2);
+    v_frequencies = flip(v_frequencies);
+    
+    v_times       = 1:size(TF_meanChans, 4);
+    v_times       = v_times - (size(TF_meanChans, 4) / 2);
+    
+    yyaxis left
+    pcolor(v_times, v_frequencies, squeeze(mean(TF_meanChans, 1)));
+    shading interp
+    colorbar;
+    % set(gca, 'clim', [minVal, maxVal])
+    ylabel('Distance from spindle peak (Hz)')
+    xlabel('Time (s)')
+    title(char(condition))
+    
+    hold on
+    
+    % Wave form plot
+    % --------------
+    yyaxis right
+    v_times = PM.cfg_seldat.latency(1) * ...
+        PM.Info.TrialParameters.s_fs  + 1 : 1 : ...
+        PM.cfg_seldat.latency(2) * ...
+        PM.Info.TrialParameters.s_fs;
+    plot(v_times, mean(WF_subject.(char(condition)), 1), ...
+        'Color',        [0, 0, 0], ...
+        'LineWidth',    2)
+    
+    i_plot = i_plot + 1;
+end
+
+
+
+%% Prepare structures for stats
+%  ------------------------------------------------------------------------
+
+load(chanlocspath) % contains 'sensors'
+
+c_fields = fieldnames(TF_series.OdorOn);
+for i_field = 1:numel(c_fields)
+    
+    if any(strcmp(c_fields(i_field), {'powspctrm', 'cfg', 'elec'}))
+        % This field is already generated and contains averaged data
+        continue
+    end
+    
+    TF_subject.OdorOn.(char(c_fields(i_field))) = ...
+        TF_series.OdorOn.(char(c_fields(i_field)));
+    TF_subject.ShamOn.(char(c_fields(i_field))) = ...
+        TF_series.ShamOn.(char(c_fields(i_field)));
+    
+end
+TF_subject.OdorOn.elec = sensors;
+TF_subject.ShamOn.elec = sensors;
+
+
+
+%% Parameters for stats
+%  ------------------------------------------------------------------------
+
+ft_defaults
+
+cfg_stats                       = [];
+%cfg_stats.latency               = [1]; % Setting this to one rejects the
+%time vector. Probably, this indicates the time vector, but it doesn't seem
+%necessary to specify since fieldtrip is taking it from structure.time
+cfg_stats.frequency             = 'all';
+cfg_stats.channel               = 'all';
+cfg_stats.correctm              = 'cluster';
+cfg_stats.method                = 'montecarlo';
+cfg_stats.statistic             = 'depsamplesT';
+% use actvsblT for activation against baseline
+cfg_stats.clusterstatistic      = 'maxsum';
+cfg_stats.minnbchan             = 2;
+
+cfg_neighb.method               = 'distance'; %'triangulation';
+cfg_neighb.neighbourdist        = 3.5; % Default is "smart guess".
+% 3.5 gets rid of edge clusters while maintaining high statistical 
+% significance in central clusters.
+cfg_neighb.channel              = 'all';
+cfg_neighb.elec                 = sensors;
+cfg_stats.neighbours            = ft_prepare_neighbours(cfg_neighb);
+
+cfg_stats.tail                  = 0; % -1, +1 or 0 for one-sided or two-sided
+cfg_stats.clustertail           = cfg_stats.tail;
+% cfg.alpha               = 0.05; % = False detection rate ?
+% Commented out in order to get p values of channels that would otherwise
+% be set to p = 1 in case the p value is higher than the alpha here.
+% Cluster perm stats --> Which clusters are "significant"
+cfg_stats.correcttail           = 'alpha'; % {'prob', 'alpha'}
+% Adapts alpha value corresponding to one-tailed or two-tailed test
+cfg_stats.numrandomization      = 100;
+cfg_stats.clusteralpha          = 0.05;  % "which chans to use for clustering" aka
+% include more or less channels
+% threshold over which a triplet is chosen
+% Design needs to be adapted to cfg.uvar and cfg.ivar
+cfg_stats.uvar                  = 1;    % condition (uvar would be the subjects)
+                                % Iactivate for cfg.statistic = 'indepsamplesT'
+cfg_stats.ivar                  = 2;
+cfg_stats.design                = [1:numel(files), ...
+                                    1:numel(files); ...
+                                    repmat(1, 1, numel(files)), ...
+                                    repmat(2, 1, numel(files))];
+                  
+cfg_stats.feedback = 'off';
+stats  = ft_freqstatistics(cfg_stats, ...
+    TF_subject.OdorOn, TF_subject.ShamOn);
+
+
+
+%% Extract statistical information /!\ under construction /!\
+%  ------------------------------------------------------------------------
+
+% Clusters and probabilities are given by channel by freq by time.
+% We will extract the channels that show significant differences and plot
+% their average TF. Then, we will overlay the time-frequency cluster part
+% that shows significant differences.
+
+% Look for clusters with probability of <= 0.2 (permissive threshold)
+p_all = stats.prob(:);
+
