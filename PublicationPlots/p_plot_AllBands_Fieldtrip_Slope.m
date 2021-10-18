cf73a4c368db27a8f8fd1a78aa5a19e9d8675ee7
Andrea <adsanchez1@uc.cl>
2021-10-18 10:06:44 -0500

    Fieldtrip plots completed. Still need readjustment to colors

--- PublicationPlots/p_plot_AllBands_Fieldtrip_Slope.m
+++ PublicationPlots/p_plot_AllBands_Fieldtrip_Slope.m
@@ -5,18 +5,8 @@ addpath('C:\Users\asanch24\Documents\MATLAB\fieldtrip-20190828\') %Windows
 
 ft_defaults
 
-% filepathD = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/DNight/'; %server
-% filepathM = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/MNight/'; %server
-
-filepathD = 'D:\TF_Calculation_90SecTrial\DNight\'; %Windows
-filepathM = 'D:\TF_Calculation_90SecTrial\MNight\'; %Windows
-
-filesOdorD = dir(strcat(filepathD,'*_Odor.mat'));
-filesVehicleD = dir(strcat(filepathD,'*_Vehicle.mat'));
-
-filesOdorM = dir(strcat(filepathM,'*_Odor.mat'));
-filesVehicleM = dir(strcat(filepathM,'*_Vehicle.mat'));
-
+% DNight = load('C:\Users\asanch24\OneDrive - St. Jude Children''s Research Hospital\Thesis_Publication\TF_Slope\TF_DNight_TFSlope.mat');
+% MNight = load('C:\Users\asanch24\OneDrive - St. Jude Children''s Research Hospital\Thesis_Publication\TF_Slope\TF_MNight_TFSlope.mat');
 
 %%
 
@@ -28,117 +18,112 @@ ThetaBand       = [4 8];
 SWBand          = [0.5 2];
 
 % load('D:\Thesis_Publication\Using\Time_Freq_Parms')
-% Time_Series_OdorD =[];
-% %--------------------------------------------------------------------------
-% % Analysis by bands
-% %--------------------------------------------------------------------------
-% for band = 1:numel(bands)
-%     
-%     cfg = [];
-%     
-%     % select frequency band
-%     if strcmp(bands(band),'Spindle')
-%         cfg.frequency = SpindleBand;
-%     end
-%     
-%     if strcmp(bands(band),'Theta')
-%         cfg.frequency = ThetaBand;
-%     end
-%     
-%     if strcmp(bands(band),'Delta')
-%         cfg.frequency = DeltaBand;
-%     end
-%     
-%     if strcmp(bands(band),'SW')
-%         cfg.frequency = SWBand;
-%     end
-%     
+Time_Series_OdorD =[];
+%--------------------------------------------------------------------------
+% Analysis by bands
+%--------------------------------------------------------------------------
+for band = 1:numel(bands)
+    
+    cfg = [];
+    
+    % select frequency band
+    if strcmp(bands(band),'Spindle')
+        cfg.frequency = SpindleBand;
+    end
+    
+    if strcmp(bands(band),'Theta')
+        cfg.frequency = ThetaBand;
+    end
+    
+    if strcmp(bands(band),'Delta')
+        cfg.frequency = DeltaBand;
+    end
+    
+    if strcmp(bands(band),'SW')
+        cfg.frequency = SWBand;
+    end
+    
 %     cfg.avgoverrpt      = 'yes';
-%     cfg.avgoverfreq     = 'yes';
-%     cfg.channel         = {'all','-E49', '-E48', '-E17', '-E128', '-E32', ...
-%         '-E1', '-E125', '-E119', '-E113', '-E56', ...
-%         '-E63', '-E68', '-E73', '-E81', '-E88', ...
-%         '-E94', '-E99', '-E107'};
-%     
-%     % Time_Series_OdorD.(bands{band}) = Time_Freq_Parms;
-%     
-%     for subj = 1:numel(filesOdorD)
-%         
-%         disp(strcat('Sujeto: ',num2str(subj)))
-%         
-%         % For D Night
-%         DNight.Odor = load(strcat(filepathD,filesOdorD(subj).name));
-%         DNight.Vehicle = load(strcat(filepathD,filesVehicleD(subj).name));
-%         
-%         DNight.Odor.Time_Freq_Odor = rmfield(DNight.Odor.Time_Freq_Odor, 'trialinfo');
-%         DNight.Vehicle.Time_Freq_Vehicle = rmfield(DNight.Vehicle.Time_Freq_Vehicle, 'trialinfo');
-%         
-%         Sel_Freq_OdorD = ft_selectdata(cfg, DNight.Odor.Time_Freq_Odor);
-%         Sel_Freq_VehicleD = ft_selectdata(cfg, DNight.Vehicle.Time_Freq_Vehicle);
-%         
-%         OdorD_trial{1,subj} = squeeze(Sel_Freq_OdorD.powspctrm);
-%         VehicleD_trial{1,subj} = squeeze(Sel_Freq_VehicleD.powspctrm);
-% 
-%         
-%         % For M Night
-%         MNight.Odor = load(strcat(filepathM,filesOdorM(subj).name));
-%         MNight.Vehicle = load(strcat(filepathM,filesVehicleM(subj).name));
-%         
-%         MNight.Odor.Time_Freq_Odor = rmfield(MNight.Odor.Time_Freq_Odor, 'trialinfo');
-%         MNight.Vehicle.Time_Freq_Vehicle = rmfield(MNight.Vehicle.Time_Freq_Vehicle, 'trialinfo');
-%         
-%         Sel_Freq_OdorM = ft_selectdata(cfg, MNight.Odor.Time_Freq_Odor);
-%         Sel_Freq_VehicleM = ft_selectdata(cfg, MNight.Vehicle.Time_Freq_Vehicle);
-%         
-%         OdorM_trial{1,subj} = squeeze(Sel_Freq_OdorM.powspctrm);
-%         VehicleM_trial{1,subj} = squeeze(Sel_Freq_VehicleM.powspctrm);
-%         
-%         time_all{1,subj} = Sel_Freq_OdorM.time;
-%     end
-%     
-%     Sel_Freq_OdorD = rmfield(Sel_Freq_OdorD,'freq');
-% %     Sel_Freq_OdorD = rmfield(Sel_Freq_OdorD, 'trialinfo');
-%     Sel_Freq_OdorD = rmfield(Sel_Freq_OdorD,'powspctrm');
-%     
-%     Sel_Freq_VehicleD = rmfield(Sel_Freq_VehicleD,'freq');
-% %     Sel_Freq_VehicleD = rmfield(Sel_Freq_VehicleD, 'trialinfo');
-%     Sel_Freq_VehicleD = rmfield(Sel_Freq_VehicleD,'powspctrm');
-%     
-%     Sel_Freq_OdorD.dimord = 'chan_time';
-%     Sel_Freq_VehicleD.dimord = 'chan_time';
-%     
-%     Sel_Freq_OdorD.time      = time_all;
-%     Sel_Freq_VehicleD.time   = time_all;
-%     
-%     Time_Series_OdorD.(bands{band}) = Sel_Freq_OdorD;
-%     Time_Series_VehicleD.(bands{band}) = Sel_Freq_VehicleD;
-%     
-%     Time_Series_OdorD.(bands{band}).trial = OdorD_trial;
-%     Time_Series_VehicleD.(bands{band}).trial = VehicleD_trial;
-% 
-%     %--- M Night---
-%     
-%     Sel_Freq_OdorM = rmfield(Sel_Freq_OdorM,'freq');
-% %     Sel_Freq_OdorM = rmfield(Sel_Freq_OdorM, 'trialinfo');
-%     Sel_Freq_OdorM = rmfield(Sel_Freq_OdorM,'powspctrm');
-%     
-%     Sel_Freq_VehicleM = rmfield(Sel_Freq_VehicleM,'freq');
-% %     Sel_Freq_VehicleM = rmfield(Sel_Freq_VehicleM, 'trialinfo');
-%     Sel_Freq_VehicleM = rmfield(Sel_Freq_VehicleM,'powspctrm');
-%     
-%     Sel_Freq_OdorM.dimord = 'chan_time';
-%     Sel_Freq_VehicleM.dimord = 'chan_time';
-%     
-%     Sel_Freq_OdorM.time      = time_all;
-%     Sel_Freq_VehicleM.time   = time_all;
-%     
-%     Time_Series_OdorM.(bands{band}) = Sel_Freq_OdorM;
-%     Time_Series_VehicleM.(bands{band}) = Sel_Freq_VehicleM;
-%     
-%     Time_Series_OdorM.(bands{band}).trial = OdorM_trial;
-%     Time_Series_VehicleM.(bands{band}).trial = VehicleM_trial;
-%      
-% end
+    cfg.avgoverfreq     = 'yes';
+    cfg.channel         = {'all','-E49', '-E48', '-E17', '-E128', '-E32', ...
+        '-E1', '-E125', '-E119', '-E113', '-E56', ...
+        '-E63', '-E68', '-E73', '-E81', '-E88', ...
+        '-E94', '-E99', '-E107'};
+    
+    % Time_Series_OdorD.(bands{band}) = Time_Freq_Parms;
+    
+    for subj = 1:numel(DNight.Time_Freq_Odor_Slope)
+        
+        disp(strcat('Sujeto: ',num2str(subj)))
+        
+        % For D Night
+        SlopeOdor = DNight.Time_Freq_Odor_Slope{subj};
+        SlopeVehicle = DNight.Time_Freq_Vehi_Slope{subj};
+        
+        Sel_Freq_OdorD = ft_selectdata(cfg, SlopeOdor);
+        Sel_Freq_VehicleD = ft_selectdata(cfg, SlopeVehicle);
+        
+        OdorD_trial{1,subj} = squeeze(Sel_Freq_OdorD.powspctrm);
+        VehicleD_trial{1,subj} = squeeze(Sel_Freq_VehicleD.powspctrm);
+
+        
+        % For M Night
+        SlopeOdor = MNight.Time_Freq_Odor_Slope{subj};
+        SlopeVehicle = MNight.Time_Freq_Vehi_Slope{subj};
+
+        
+        Sel_Freq_OdorM = ft_selectdata(cfg, SlopeOdor);
+        Sel_Freq_VehicleM = ft_selectdata(cfg, SlopeVehicle);
+        
+        OdorM_trial{1,subj} = squeeze(Sel_Freq_OdorM.powspctrm);
+        VehicleM_trial{1,subj} = squeeze(Sel_Freq_VehicleM.powspctrm);
+        
+        time_all{1,subj} = Sel_Freq_OdorM.time;
+    end
+    
+    Sel_Freq_OdorD = rmfield(Sel_Freq_OdorD,'freq');
+%     Sel_Freq_OdorD = rmfield(Sel_Freq_OdorD, 'trialinfo');
+    Sel_Freq_OdorD = rmfield(Sel_Freq_OdorD,'powspctrm');
+    
+    Sel_Freq_VehicleD = rmfield(Sel_Freq_VehicleD,'freq');
+%     Sel_Freq_VehicleD = rmfield(Sel_Freq_VehicleD, 'trialinfo');
+    Sel_Freq_VehicleD = rmfield(Sel_Freq_VehicleD,'powspctrm');
+    
+    Sel_Freq_OdorD.dimord = 'chan_time';
+    Sel_Freq_VehicleD.dimord = 'chan_time';
+    
+    Sel_Freq_OdorD.time      = time_all;
+    Sel_Freq_VehicleD.time   = time_all;
+    
+    Time_Series_OdorD.(bands{band}) = Sel_Freq_OdorD;
+    Time_Series_VehicleD.(bands{band}) = Sel_Freq_VehicleD;
+    
+    Time_Series_OdorD.(bands{band}).trial = OdorD_trial;
+    Time_Series_VehicleD.(bands{band}).trial = VehicleD_trial;
+
+    %--- M Night---
+    
+    Sel_Freq_OdorM = rmfield(Sel_Freq_OdorM,'freq');
+%     Sel_Freq_OdorM = rmfield(Sel_Freq_OdorM, 'trialinfo');
+    Sel_Freq_OdorM = rmfield(Sel_Freq_OdorM,'powspctrm');
+    
+    Sel_Freq_VehicleM = rmfield(Sel_Freq_VehicleM,'freq');
+%     Sel_Freq_VehicleM = rmfield(Sel_Freq_VehicleM, 'trialinfo');
+    Sel_Freq_VehicleM = rmfield(Sel_Freq_VehicleM,'powspctrm');
+    
+    Sel_Freq_OdorM.dimord = 'chan_time';
+    Sel_Freq_VehicleM.dimord = 'chan_time';
+    
+    Sel_Freq_OdorM.time      = time_all;
+    Sel_Freq_VehicleM.time   = time_all;
+    
+    Time_Series_OdorM.(bands{band}) = Sel_Freq_OdorM;
+    Time_Series_VehicleM.(bands{band}) = Sel_Freq_VehicleM;
+    
+    Time_Series_OdorM.(bands{band}).trial = OdorM_trial;
+    Time_Series_VehicleM.(bands{band}).trial = VehicleM_trial;
+     
+end
 %%
 filename     = 'C:\Users\asanch24\OneDrive - St. Jude Children''s Research Hospital\Thesis_Publication\Using\dummyfile.set'; %windows
 %filename     = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/dummyfile.set'; %server
@@ -149,11 +134,11 @@ Nsub = 23;
 
 
 
-for band = 1%numel(bands)
+for band = 1:numel(bands)
     
     cfg                  = [];
     cfg.method           = 'montecarlo';
-    cfg.statistic        = 'depsamplesT';
+    cfg.statistic        = 'indepsamplesT';
     cfg.correctm         = 'cluster'; % correction
     cfg.clusteralpha     = 0.05;
     cfg.clusterstatistic = 'maxsum';
@@ -164,7 +149,7 @@ for band = 1%numel(bands)
     cfg.tail             = 0;
     cfg.clustertail      = 0;
     cfg.alpha            = 0.025;
-    cfg.numrandomization = 1000;
+    cfg.numrandomization = 10000;
     
     
     Nsubj  = Nsub;
@@ -262,11 +247,11 @@ for band = 1%numel(bands)
     % plot for OdorD_VehicleD
     %----------------------------------
     
-    timestep      = 1; % timestep between time windows for each subplot (in seconds)
+    timestep      = 0.5; % timestep between time windows for each subplot (in seconds)
     sampling_rate = 10; % Data has a temporal resolution of 300 Hz
     sample_count  = length(stat_OdorD_VehicleD.time);
     % number of temporal samples in the statistics object
-    j = -2:timestep:3; % Temporal endpoints (in seconds) of the ERP average computed in each subplot
+    j = -5:timestep:5; % Temporal endpoints (in seconds) of the ERP average computed in each subplot
     m = 1:timestep*sampling_rate:sample_count; % temporal endpoints in M/EEG samples
 
 
@@ -276,9 +261,9 @@ for band = 1%numel(bands)
     [i1,i2] = match_str(Time_Series_OdorD.(bands{band}).label, stat_OdorD_VehicleD.label);
     
     for k = 1:numel(j)-1
-        subplot(4,8,k);
-        %subplot(3,7,k);
-        subplot(6,9,k);
+        %subplot(4,8,k);
+        subplot(3,7,k);
+%         subplot(6,9,k);
         
         cfg = [];
         cfg.xlim = [j(k) j(k+1)];   % time interval of the subplot
@@ -330,8 +315,8 @@ for band = 1%numel(bands)
     
     for k = 1:numel(j)-1
         %subplot(4,8,k);
-        %subplot(3,7,k);
-        subplot(6,9,k);
+        subplot(3,7,k);
+%         subplot(6,9,k);
         
         cfg = [];
         cfg.xlim = [j(k) j(k+1)];   % time interval of the subplot
@@ -382,8 +367,8 @@ for band = 1%numel(bands)
     
     for k = 1:numel(j)-1
         %subplot(4,8,k);
-        %subplot(3,7,k);
-        subplot(6,9,k);
+        subplot(3,7,k);
+%         subplot(6,9,k);
         
         cfg = [];
         cfg.xlim = [j(k) j(k+1)];   % time interval of the subplot
