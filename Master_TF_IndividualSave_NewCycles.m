f90fded1919ec319e46f0d0ac85b20ff23c98b31
ascorzo <adsanchez1@uc.cl>
2021-11-02 20:32:34 -0300

    new cycles in server

--- Master_TF_IndividualSave_NewCycles.m
+++ Master_TF_IndividualSave_NewCycles.m
@@ -1,8 +1,8 @@
-% addpath('/home/andrea/Documents/MatlabFunctions/fieldtrip-20200828/')
-addpath('C:\Users\asanch24\Documents\MATLAB\fieldtrip-20190828\')
+addpath('/home/andrea/Documents/MatlabFunctions/fieldtrip-20200828/')
+% addpath('C:\Users\asanch24\Documents\MATLAB\fieldtrip-20190828\')
 
 % ft_defaults
-% addpath('/home/andrea/Documents/MatlabFunctions/fieldtrip-20200828/qsub')
+addpath('/home/andrea/Documents/MatlabFunctions/fieldtrip-20200828/qsub')
 ft_warning off
 
 %--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ cfg_Tf.method               = 'wavelet';
 cfg_Tf.output               = 'pow';
 cfg_Tf.foi                  = minfreq:s_fstep:maxfreq; 
 % cfg_Tf.width                = cycles;
-v_timeWindows = 5:(-4.5/numel(cfg_Tf.foi)):0.5;
+v_timeWindows               = 5:(-4.5/numel(cfg_Tf.foi)):0.5;
 cfg_Tf.t_ftimwin            = v_timeWindows;   
 cfg_Tf.toi                  = -11:s_tstep:66; 
 % toi this is extended before and after to deal with border effect of wavelet
@@ -34,11 +34,11 @@ cfg_Tf.keeptrials           = 'yes';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% filepath = '/mnt/disk1/andrea/German_Study/Data/PreProcessed/Epoched_90SecTrial_MastoidRef-Interp_NewEpochs/DNight/';
-% savepath = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/DNight/';
+filepath = '/mnt/disk1/andrea/German_Study/Data/PreProcessed/Epoched_90SecTrial_MastoidRef-Interp_NewEpochs/DNight/';
+savepath = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/DNight/';
 
-filepath = 'D:\Thesis_Publication\Epoched_90SecTrial_MastoidRef-Interp_NewEpochs\DNight\';
-savepath = 'D:\TF_Calculation_90SecTrial\';
+% filepath = 'D:\Thesis_Publication\Epoched_90SecTrial_MastoidRef-Interp_NewEpochs\DNight\';
+% savepath = 'D:\TF_Calculation_90SecTrial\';
 
 files = dir(strcat(filepath,'*.set'));
 
@@ -95,75 +95,75 @@ for subj = 1:numel(files)
     Time_Freq_Vehicle = ft_selectdata(cfg, Time_Freq_Baseline);
     Time_Freq_Vehicle.time = Time_Freq_Odor.time;
 
-    save(strcat(savepath,files(subj).name(1:12),'TF_DN_Odor'),'Time_Freq_Odor','-v7.3')
-    save(strcat(savepath,files(subj).name(1:12),'TF_DN_Vehicle'),'Time_Freq_Vehicle','-v7.3')
+    save(strcat(savepath,files(subj).name(1:12),'TF_NewCycles_DN_Odor'),'Time_Freq_Odor','-v7.3')
+    save(strcat(savepath,files(subj).name(1:12),'TF_NewCycles_DN_Vehicle'),'Time_Freq_Vehicle','-v7.3')
 
 end
 
-% %% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % For Motor Associated Odor Night
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% 
-% filepath = '/mnt/disk1/andrea/German_Study/Data/PreProcessed/Epoched_90SecTrial_MastoidRef-Interp_NewEpochs/MNight/';
-% savepath = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/MNight/';
-% 
-% files = dir(strcat(filepath,'*.set'));
-% 
-% 
-% for subj = 1:numel(files)
-%     
-%     %______________________________________________________________________
-%     %
-%     %Cue Odor
-%     %______________________________________________________________________
-%     
-%     disp(strcat('Sujeto: ',num2str(subj)))
-%     disp(strcat('Cue'))
-%     
-%     %--------- Load Data --------------------------------------------------
-%     addpath(genpath('/home/andrea/Documents/MatlabFunctions/eeglab2019_1/'))
-%     EEGOdor = pop_loadset(strcat(filepath,files(subj).name));
-%     dataOdor = eeglab2fieldtrip(EEGOdor,'raw');
-%     rmpath(genpath('/home/andrea/Documents/MatlabFunctions/eeglab2019_1/'))
-%     
-%     
-%     %-----------Time-Frequency Calculation---------------------------------
-%     
-%     Time_Freq_DA_Temp  = ft_freqanalysis(cfg_Tf, dataOdor);
-%     
-%     clear EEGOdor dataOdor
-%     
-%     %--------------Select time of interest removing borders-----------------
-%     cfg = [];
-%     cfg.latency = [-5 60];
-%     Time_Freq = ft_selectdata(cfg, Time_Freq_DA_Temp);
-%     
-%     clear Time_Freq_DA_Temp
-%     
-%     %----------------------------------------------------------------------
-%     % baseline correction
-%     %----------------------------------------------------------------------
-%     cfg_Bas                     = [];
-%     cfg_Bas.baseline            = [-5 60];
-%     cfg_Bas.baselinetype        = 'zscore';
-%     Time_Freq_Baseline = ft_freqbaseline(cfg_Bas,Time_Freq);
-%     
-%     %----------------------------------------------------------------------
-%     % Separate conditions
-%     %----------------------------------------------------------------------
-%     cfg = [];
-%     cfg.latency = [-5 30];
-%     Time_Freq_Odor = ft_selectdata(cfg, Time_Freq_Baseline);
-%     
-%     cfg = [];
-%     cfg.latency = [25 60];
-%     Time_Freq_Vehicle = ft_selectdata(cfg, Time_Freq_Baseline);
-%     Time_Freq_Vehicle.time = Time_Freq_Odor.time;
-% 
-%     save(strcat(savepath,files(subj).name(1:12),'TF_MN_Odor'),'Time_Freq_Odor','-v7.3')
-%     save(strcat(savepath,files(subj).name(1:12),'TF_MN_Vehicle'),'Time_Freq_Vehicle','-v7.3')
-% 
-% end
-% 
+%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% For Motor Associated Odor Night
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+filepath = '/mnt/disk1/andrea/German_Study/Data/PreProcessed/Epoched_90SecTrial_MastoidRef-Interp_NewEpochs/MNight/';
+savepath = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/MNight/';
+
+files = dir(strcat(filepath,'*.set'));
+
+
+for subj = 1:numel(files)
+    
+    %______________________________________________________________________
+    %
+    %Cue Odor
+    %______________________________________________________________________
+    
+    disp(strcat('Sujeto: ',num2str(subj)))
+    disp(strcat('Cue'))
+    
+    %--------- Load Data --------------------------------------------------
+    addpath(genpath('/home/andrea/Documents/MatlabFunctions/eeglab2019_1/'))
+    EEGOdor = pop_loadset(strcat(filepath,files(subj).name));
+    dataOdor = eeglab2fieldtrip(EEGOdor,'raw');
+    rmpath(genpath('/home/andrea/Documents/MatlabFunctions/eeglab2019_1/'))
+    
+    
+    %-----------Time-Frequency Calculation---------------------------------
+    
+    Time_Freq_DA_Temp  = ft_freqanalysis(cfg_Tf, dataOdor);
+    
+    clear EEGOdor dataOdor
+    
+    %--------------Select time of interest removing borders-----------------
+    cfg = [];
+    cfg.latency = [-5 60];
+    Time_Freq = ft_selectdata(cfg, Time_Freq_DA_Temp);
+    
+    clear Time_Freq_DA_Temp
+    
+    %----------------------------------------------------------------------
+    % baseline correction
+    %----------------------------------------------------------------------
+    cfg_Bas                     = [];
+    cfg_Bas.baseline            = [-5 60];
+    cfg_Bas.baselinetype        = 'zscore';
+    Time_Freq_Baseline = ft_freqbaseline(cfg_Bas,Time_Freq);
+    
+    %----------------------------------------------------------------------
+    % Separate conditions
+    %----------------------------------------------------------------------
+    cfg = [];
+    cfg.latency = [-5 30];
+    Time_Freq_Odor = ft_selectdata(cfg, Time_Freq_Baseline);
+    
+    cfg = [];
+    cfg.latency = [25 60];
+    Time_Freq_Vehicle = ft_selectdata(cfg, Time_Freq_Baseline);
+    Time_Freq_Vehicle.time = Time_Freq_Odor.time;
+
+    save(strcat(savepath,files(subj).name(1:12),'TF_NewCycles_MN_Odor'),'Time_Freq_Odor','-v7.3')
+    save(strcat(savepath,files(subj).name(1:12),'TF_NewCycles_MN_Vehicle'),'Time_Freq_Vehicle','-v7.3')
+
+end
+
--- p_TF_OverTrials_Stim.m
+++ p_TF_OverTrials_Stim.m
@@ -5,87 +5,87 @@ addpath('/home/andrea/Documents/MatlabFunctions/fieldtrip-20200828/qsub')
 
 ft_warning off
 
-% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % % Odor Night
-% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% filepath = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/DNight/';
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Odor Night
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+filepath = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/DNight/';
 
-% %files = dir(strcat(filepath,'*.set'));
-% filesOdor = dir(strcat(filepath,'*_Odor.mat'));
-% filesVehicle = dir(strcat(filepath,'*_Vehicle.mat'));
-% p_clustersOfInterest
-% clusters = fieldnames(Clust);
+%files = dir(strcat(filepath,'*.set'));
+filesOdor = dir(strcat(filepath,'*NewCycles*_Odor.mat'));
+filesVehicle = dir(strcat(filepath,'*NewCycles*_Vehicle.mat'));
+p_clustersOfInterest
+clusters = fieldnames(Clust);
 
 
-% DNight_Cycles = load('/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/OdorCycles_DNight');
+DNight_Cycles = load('/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/OdorCycles_DNight');
 
-% for subj = 1:numel(filesOdor)
+for subj = 1:numel(filesOdor)
     
-%     v_cycles =  DNight_Cycles.OdorCyles.(filesOdor(subj).name(1:6));
+    v_cycles =  DNight_Cycles.OdorCyles.(filesOdor(subj).name(1:6));
     
-%     [s_MaxNumStims, s_MaxNumStimsIdx] = max(v_cycles);
+    [s_MaxNumStims, s_MaxNumStimsIdx] = max(v_cycles);
     
-%     s_Firstepoch  = s_MaxNumStimsIdx-s_MaxNumStims+1;
-%     s_Lastepoch = s_MaxNumStimsIdx;
+    s_Firstepoch  = s_MaxNumStimsIdx-s_MaxNumStims+1;
+    s_Lastepoch = s_MaxNumStimsIdx;
     
-%     load(strcat(filepath,filesOdor(subj).name));
-%     load(strcat(filepath,filesVehicle(subj).name));
+    load(strcat(filepath,filesOdor(subj).name));
+    load(strcat(filepath,filesVehicle(subj).name));
     
-%     cfg                         = [];
-%     cfg.trials                  = s_Firstepoch:s_Lastepoch;
-%     Time_Freq_Odor_Selected     = ft_selectdata(cfg, Time_Freq_Odor);
-%     Time_Freq_Vehi_Selected     = ft_selectdata(cfg, Time_Freq_Vehicle);
+    cfg                         = [];
+    cfg.trials                  = s_Firstepoch:s_Lastepoch;
+    Time_Freq_Odor_Selected     = ft_selectdata(cfg, Time_Freq_Odor);
+    Time_Freq_Vehi_Selected     = ft_selectdata(cfg, Time_Freq_Vehicle);
     
-%     for freq = 1:numel(Time_Freq_Odor_Selected.freq)
-%         for chan = 1:numel(Time_Freq_Odor_Selected.label)
+    for freq = 1:numel(Time_Freq_Odor_Selected.freq)
+        for chan = 1:numel(Time_Freq_Odor_Selected.label)
 
-%             disp(strcat('Sujeto: ',num2str(subj),{' '},...
-%                 'Frequencia: ',num2str(freq),{' '},...
-%                 'Channel: ',num2str(chan)))
+            disp(strcat('Sujeto: ',num2str(subj),{' '},...
+                'Frequencia: ',num2str(freq),{' '},...
+                'Channel: ',num2str(chan)))
 
-%             for t = 1:numel(Time_Freq_Odor_Selected.time)
+            for t = 1:numel(Time_Freq_Odor_Selected.time)
 
-%                 Xt = 1:size(Time_Freq_Odor_Selected.powspctrm,1);
+                Xt = 1:size(Time_Freq_Odor_Selected.powspctrm,1);
 
                 
-%                 OdorEvol    = squeeze(Time_Freq_Odor_Selected.powspctrm(:,chan,freq,t));
-%                 VehicleEvol = squeeze(Time_Freq_Vehi_Selected.powspctrm(:,chan,freq,t));
+                OdorEvol    = squeeze(Time_Freq_Odor_Selected.powspctrm(:,chan,freq,t));
+                VehicleEvol = squeeze(Time_Freq_Vehi_Selected.powspctrm(:,chan,freq,t));
                 
-%                 mdl_Odor = fitlm(Xt,OdorEvol, 'linear');
-%                 mdl_Vehicle  = fitlm(Xt,VehicleEvol, 'linear');
+                mdl_Odor = fitlm(Xt,OdorEvol, 'linear');
+                mdl_Vehicle  = fitlm(Xt,VehicleEvol, 'linear');
                 
-%                 % Get slope of regression
-%                 slope_DNight_Odor(chan,freq,t) = (mdl_Odor.Fitted(end) - mdl_Odor.Fitted(1) ) / ...
-%                     (numel(Xt) - 1);
+                % Get slope of regression
+                slope_DNight_Odor(chan,freq,t) = (mdl_Odor.Fitted(end) - mdl_Odor.Fitted(1) ) / ...
+                    (numel(Xt) - 1);
                 
-%                 slope_DNight_Vehicle(chan,freq,t) = (mdl_Vehicle.Fitted(end) - mdl_Vehicle.Fitted(1) ) / ...
-%                     (numel(Xt) - 1);
-%             end
-%         end
-%     end
+                slope_DNight_Vehicle(chan,freq,t) = (mdl_Vehicle.Fitted(end) - mdl_Vehicle.Fitted(1) ) / ...
+                    (numel(Xt) - 1);
+            end
+        end
+    end
     
-%     cfg                         = [];
-%     cfg.trials                  ='all';
-%     cfg.avgoverrpt              ='yes';
-%     Time_Freq_Odor_Slope{subj}  = ft_selectdata(cfg, Time_Freq_Odor);
-%     Time_Freq_Vehi_Slope{subj}  = ft_selectdata(cfg, Time_Freq_Vehicle);
+    cfg                         = [];
+    cfg.trials                  ='all';
+    cfg.avgoverrpt              ='yes';
+    Time_Freq_Odor_Slope{subj}  = ft_selectdata(cfg, Time_Freq_Odor);
+    Time_Freq_Vehi_Slope{subj}  = ft_selectdata(cfg, Time_Freq_Vehicle);
 
-%     % Replace
-%     Time_Freq_Odor_Slope{subj}.powspctrm  = slope_DNight_Odor;
-%     Time_Freq_Vehi_Slope{subj}.powspctrm  = slope_DNight_Vehicle;
+    % Replace
+    Time_Freq_Odor_Slope{subj}.powspctrm  = slope_DNight_Odor;
+    Time_Freq_Vehi_Slope{subj}.powspctrm  = slope_DNight_Vehicle;
 
-%     clear slope_DNight_Odor slope_DNight_Vehicle
+    clear slope_DNight_Odor slope_DNight_Vehicle
 
-%     savepath ='/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/';
+    savepath ='/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/';
 
-%     save(strcat(savepath,'TF_DNight_TFSlope'),...
-%         'Time_Freq_Odor_Slope','Time_Freq_Vehi_Slope')
+    save(strcat(savepath,'NewCycles_TF_DNight_TFSlope'),...
+        'Time_Freq_Odor_Slope','Time_Freq_Vehi_Slope')
     
-% end
+end
 
 
 
-% clear Time_Freq_Odor_Slope Time_Freq_Vehi_Slope 
+clear Time_Freq_Odor_Slope Time_Freq_Vehi_Slope 
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Motor Night
@@ -94,8 +94,9 @@ ft_warning off
 filepath = '/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/MNight/';
 
 %files = dir(strcat(filepath,'*.set'));
-filesOdor = dir(strcat(filepath,'*_Odor.mat'));
-filesVehicle = dir(strcat(filepath,'*_Vehicle.mat'));
+filesOdor = dir(strcat(filepath,'*NewCycles*_Odor.mat'));
+filesVehicle = dir(strcat(filepath,'*NewCycles*_Vehicle.mat'));
+
 p_clustersOfInterest
 clusters = fieldnames(Clust);
 
@@ -158,7 +159,7 @@ for subj = 1:numel(filesOdor)
 
     savepath ='/mnt/disk1/andrea/German_Study/Time_Frequency_FT/TF_Calculation_90SecTrial/';
 
-    save(strcat(savepath,'TF_MNight_TFSlope'),...
+    save(strcat(savepath,'NewCycles_TF_MNight_TFSlope'),...
         'Time_Freq_Odor_Slope','Time_Freq_Vehi_Slope')
     
 end
